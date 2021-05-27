82b3ccc7ce4d3e23f81eb567c1f654f6c39ab227
liyueyao1015 <3316277393@qq.com>
2021-05-27 16:12:40 +0800

    身份认证
    add:Auth_Ticket_Response 消息处理函数

--- src/main/java/message/message_handler/handler/AuthKeyResponseHandler.java
+++ src/main/java/message/message_handler/handler/AuthKeyResponseHandler.java
@@ -3,13 +3,29 @@ package message.message_handler.handler;
 import message.Message;
 import message.response.AuthKeyResponse;
 import message.response.AuthTicketResponse;
+import module.ticket.TicketGrantingTicket;
 import network.MessageSender;
 
 import java.util.Set;
 
 public class AuthKeyResponseHandler extends MessageHandler{
     boolean handle(AuthKeyResponse authKeyResponse, AuthTicketResponse authTicketResponse, MessageSender sender){
-        return false;
+        //Auth_Ticket_Response 消息处理函数
+        int n=authKeyResponse.getCode();
+        switch (n){
+            case 1:{
+                //调用静态UI函数，内存为认证失败，用户不存在
+                break;
+            }
+            case 0 : {
+                TicketGrantingTicket TGT= authTicketResponse.getTGT();
+                authTicketResponse.setTGT(TGT);
+                break;
+            }
+            default:
+                return false;
+        }
+        return true;
     }
 
     @Override
