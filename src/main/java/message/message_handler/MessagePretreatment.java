3595234939f0c6346d007bfde4f2f04254c2e2a3
HaotianCao <44105807+HaotianCao@users.noreply.github.com>
2021-05-27 14:28:42 +0800

    Update MessagePretreatment.java
    add: type-stage , stage-set<type>

--- src/main/java/message/message_handler/MessagePretreatment.java
+++ src/main/java/message/message_handler/MessagePretreatment.java
@@ -15,19 +15,53 @@ public class MessagePretreatment {
     static {
 
         messageTypeStageMap = new HashMap<>();
-
-
         messageTypeStageMap.put(MessageType.AUTH_LOGIN_REQUEST, IdentificationStage.AUTH_REQUEST);
-
-
-
-
+        messageTypeStageMap.put(MessageType.ACCESS_TICKET_REQUEST,IdentificationStage.ACCESS_REQUEST);
+        messageTypeStageMap.put(MessageType.ACCESS_REQUEST_VERIFY_REQUEST,IdentificationStage.ACCESS_REQUEST);
+        messageTypeStageMap.put(MessageType.SERVICE_TICKET_REQUEST,IdentificationStage.SERVICE_REQUEST);
+        messageTypeStageMap.put(MessageType.SERVICE_REQUEST_VERIFY_REQUEST,IdentificationStage.SERVICE_REQUEST);
+        messageTypeStageMap.put(MessageType.ACCESS_KEY_RESPONSE,IdentificationStage.ACCESS_RESPONSE);
+        messageTypeStageMap.put(MessageType.AUTH_KEY_RESPONSE,IdentificationStage.AUTH_RESPONSE);
+        messageTypeStageMap.put(MessageType.ACCESS_TICKET_RESPONSE,IdentificationStage.ACCESS_RESPONSE);
+        messageTypeStageMap.put(MessageType.AUTH_TICKET_RESPONSE,IdentificationStage.AUTH_RESPONSE);
+        messageTypeStageMap.put(MessageType.SERVICE_CONFIRM_RESPONSE,IdentificationStage.SERVICE_RESPONSE);
 
         messageStageSetMap = new HashMap<>();
 
+        HashSet<MessageType> messageTypes = new HashSet<>();
+        messageTypes.add(MessageType.AUTH_LOGIN_REQUEST);
+        messageStageSetMap.put(IdentificationStage.AUTH_REQUEST, messageTypes);
+
+        messageTypes = new HashSet<>();
+        messageTypes.add(MessageType.AUTH_KEY_RESPONSE);
+        messageTypes.add(MessageType.AUTH_TICKET_RESPONSE);
+        messageStageSetMap.put(IdentificationStage.AUTH_RESPONSE, messageTypes);
+
+        messageTypes=new HashSet<>();
+        messageTypes.add(MessageType.ACCESS_TICKET_REQUEST);
+        messageTypes.add(MessageType.ACCESS_REQUEST_VERIFY_REQUEST);
+        messageStageSetMap.put(IdentificationStage.ACCESS_REQUEST,messageTypes);
+
+        messageTypes=new HashSet<>();
+        messageTypes.add(MessageType.ACCESS_TICKET_RESPONSE);
+        messageTypes.add(MessageType.ACCESS_KEY_RESPONSE);
+        messageStageSetMap.put(IdentificationStage.ACCESS_RESPONSE,messageTypes);
+
+        messageTypes=new HashSet<>();
+        messageTypes.add(MessageType.SERVICE_TICKET_REQUEST);
+        messageTypes.add(MessageType.SERVICE_REQUEST_VERIFY_REQUEST);
+        messageStageSetMap.put(IdentificationStage.SERVICE_REQUEST,messageTypes);
+
+        messageTypes=new HashSet<>();
+        messageTypes.add(MessageType.SERVICE_CONFIRM_RESPONSE);
+        messageStageSetMap.put(IdentificationStage.SERVICE_RESPONSE,messageTypes);
 
 
-        messageStageSetMap.put(IdentificationStage.AUTH_REQUEST, new HashSet<>(Collections.singleton(MessageType.AUTH_LOGIN_REQUEST)));
+
+
+
+
+
 
 
 
