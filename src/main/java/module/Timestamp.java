24f7f9188dcc9ccbf568aed3ab7f5ef28fd3254b
k7ol <56666912+rk7ol@users.noreply.github.com>
2021-06-02 16:50:50 +0800

    timestamp encrypt

--- src/main/java/module/Timestamp.java
+++ src/main/java/module/Timestamp.java
@@ -1,6 +1,25 @@
 package module;
 
-public class Timestamp extends CryptUnit{
+import util.serialization.JacksonDecoder;
+
+import java.io.IOException;
+
+public class Timestamp extends CryptUnit {
+
+    private static final JacksonDecoder<Timestamp> timestampJacksonDecoder = new JacksonDecoder<Timestamp>() {
+        @Override
+        public Timestamp decode(byte[] bytes) {
+
+            try {
+                return getObjectMapper().readValue(bytes, Timestamp.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+    };
 
     private long timestamp;
 
@@ -10,5 +29,20 @@ public class Timestamp extends CryptUnit{
 
     public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
+    }
+
+    @Override
+    protected void decrypt(byte[] key) {
+
+
+        byte[] plaintext = DECRYPTOR.decrypt(getCiphertext(), key);
+
+        setCryptState(CryptState.DECRYPTED);
+
+        Timestamp timestamp = timestampJacksonDecoder.decode(plaintext);
+
+        this.timestamp = timestamp.timestamp;
+
+
     }
 }
